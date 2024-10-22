e19fd92b9b3c15cd88020ade2aeec023070e6949
Ivan Gibson <ivangibson93@gmail.com>
2024-10-22 11:38:00 -0300

    Atualizações

--- projeto_django/urls.py
+++ projeto_django/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.http import HttpResponse
+from recipes.views import contato, home, sobre 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), #home
+    path('sobre/', sobre), # /sobre/
+    path('contato/', contato), # /contato/
 ]
